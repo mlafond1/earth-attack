2404c61b8d961882271109f899f42a9ff63a63c4
Mathieu Lafond <lafond.mathieu.2@courrier.uqam.ca>
2019-03-13 23:21:34 -0400

    new: debut script placement sur map

---
+++ Assets/placeStructureOnMap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class placeStructureOnMap : MonoBehaviour
+{
+
+    GameObject nextStructure;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(nextStructure != null){
+            if (Input.GetMouseButtonDown(0)){
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit = new RaycastHit();
+                if (Physics.Raycast(ray, out hit)){
+                    Vector3 hitPosition = hit.collider.transform.position;
+                    Debug.Log("Mouse Down hit: "+hitPosition);
+                    nextStructure.transform.position = new Vector3(hitPosition.x,0,hitPosition.z);
+                    // Do something
+                    nextStructure = null;
+                }
+            }
+        }
+    }
+}
