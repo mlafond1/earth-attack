f94496778d96027dac44ae0fb36519002cf07145
Mathieu Lafond <lafond.mathieu.2@courrier.uqam.ca>
2019-03-14 23:35:34 -0400

    new: SelectStructure to place on UI button

--- Assets/placeStructureOnMap.cs
+++ Assets/placeStructureOnMap.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class placeStructureOnMap : MonoBehaviour
+public class PlaceStructureOnMap : MonoBehaviour
 {
-
-    GameObject nextStructure;
-    int dimensionTileMap = 11;
-    float tileDimension_x = 0;
-    float tileDimension_z = 0;
+    private static int DEFAULT_DIMENSION = 11;
+    private static GameObject nextStructure;
+    int dimensionTileMap;
+    bool[,] tileMap;
+    float tileDimension_x;
+    float tileDimension_z;
 
     // Start is called before the first frame update
     void Start()
     {
-        nextStructure = GameObject.Find("constructionSol_1");
+        //nextStructure = GameObject.Find("constructionSol_1");
+        bool[,] defaultTiles = new bool[DEFAULT_DIMENSION, DEFAULT_DIMENSION];
+        SetUpMap(defaultTiles, DEFAULT_DIMENSION);
+    }
+
+    public static void ChangeStructure(GameObject structure){
+        nextStructure = structure;
+    }
+
+    public void SetUpMap(bool[,] tiles, int length){
+        dimensionTileMap = length;
+        tileMap = tiles;
         if(GetComponent<Collider>() == null) 
             gameObject.AddComponent<BoxCollider>();
         tileDimension_x = GetComponent<Collider>().bounds.size.x/dimensionTileMap;
@@ -35,13 +47,24 @@ public class placeStructureOnMap : MonoBehaviour
                 float pos_x = (index_x * tileDimension_x) + tileDimension_x/2f - hit.collider.bounds.size.x/2f;
                 float pos_z = (index_z * tileDimension_z) + tileDimension_z/2f - hit.collider.bounds.size.z/2f;
 
-                nextStructure.transform.position = new Vector3(pos_x, hit.point.y, pos_z);
-                
-                Debug.Log("Object is at index " + new Vector2(index_x, index_z));
-                Debug.Log("Object now at " + nextStructure.transform.position);
-                
-                // nextStructure = null;
+                if(tileMap[index_x, index_z]){ // Tile Occupied
+                    Debug.Log("Tile already occupied");
+                }
+                else { // Tile Open
+                    GameObject clone = Instantiate(nextStructure, new Vector3(pos_x, hit.point.y, pos_z), nextStructure.transform.rotation);
+                    clone.name = nextStructure.name + "("+index_x+"," +index_z+")";
+                    Debug.Log("Object is at index " + new Vector2(index_x, index_z));
+                    Debug.Log("Object now at " + nextStructure.transform.position);
+                    tileMap[index_x, index_z] = true;
+                    // Pourrait occasionner un coût en ressource (in game)
+                    // Remove Selection After usage
+                    // nextStructure = null;
+                }
             }
+        }
+        // Cancel Selection
+        if(Input.GetKeyDown(KeyCode.Q)){
+            nextStructure = null;
         }
     }
 }
