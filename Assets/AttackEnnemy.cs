eab46ef39a1b676e320168fbe656f2716d6afff3
Mathieu Lafond <lafond.mathieu.2@courrier.uqam.ca>
2019-03-16 16:46:42 -0400

    new: AttackEnemy, EnemyHealth + enemy death

---
+++ Assets/AttackEnnemy.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackEnnemy : MonoBehaviour
+{
+
+    float attackRange;
+    float attackDamage;
+    float attackRate;
+    float nextAttackTimer;
+    Vector3 position;
+    EnemyHealth focus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackRange = 10f;
+        attackDamage = 10f;
+        attackRate = 0.25f;  // 1 = 1 sec
+        nextAttackTimer = 0f;
+        position = transform.position;
+    }
+
+    bool isInRadius(Vector3 positionEnemy, out float distance){
+        distance = Vector3.Distance(position, positionEnemy);
+        return distance <= attackRange;
+    }
+    bool isInRadius(Vector3 positionEnemy){
+        float distance = 0f;
+        return isInRadius(positionEnemy, out distance);
+    }
+
+    void AcquireTarget(){
+        EnemyHealth closest = null;
+        float closestDistance = float.MaxValue;
+        EnemyHealth[] enemyHealths = GameObject.FindObjectsOfType<EnemyHealth>();
+        foreach (EnemyHealth enemyHealth in enemyHealths) {
+            float distance = 0;
+            if(isInRadius(enemyHealth.transform.position, out distance)){
+                if(distance < closestDistance){
+                    closestDistance = distance;
+                    closest = enemyHealth;
+                }
+            }
+        }
+        if(closest != null){
+            focus = closest;
+            nextAttackTimer = 0;
+            Debug.Log(name + " has acquired Target " + focus);
+        }
+    }
+
+    void AttackTarget(){
+        nextAttackTimer += Time.deltaTime;
+        if(nextAttackTimer >= attackRate){
+            nextAttackTimer = 0;
+            Debug.Log(name + " is attacking " + focus.name);
+            focus.TakeDamage(attackDamage);
+        }
+    }
+
+    void LoseFocus(){
+        focus = null;
+        Debug.Log(name + " lost Target");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(focus == null){
+            AcquireTarget();
+        }
+        else if (isInRadius(focus.transform.position)) {
+            AttackTarget();
+        }
+        else {
+            LoseFocus();
+        }
+    }
+}
---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    float health;
+
+    public void TakeDamage(float damage){
+        health -= damage;
+        if(health <= 0) Death();
+    }
+
+    public void Death(){
+        Debug.Log(name + " died");
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
