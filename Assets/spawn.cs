8d77630a4a3e25a46fc8b1f72c7edc3e7a80fdcb
akimtouat <akim.l.touat@gmail.com>
2019-03-19 00:15:28 -0400

    Script spawn simple

---
+++ Assets/spawn.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class spawn : MonoBehaviour
+{
+    public Transform ennemy;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+    private float time = 30f; 
+
+
+    private int waveIndex = 1;
+
+    void Update()
+    {
+        if(countdown <= 0f && time >= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        time -= Time.deltaTime;
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        
+    }
+
+    void SpawnEnnemy()
+    {
+        Instantiate(ennemy, spawnPoint.position, spawnPoint.rotation);
+    }
+}
