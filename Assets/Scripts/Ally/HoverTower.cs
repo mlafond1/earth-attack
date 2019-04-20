7f9a869278335ea151e0361b4ae2ce139a02be2b
Mathieu Lafond <lafond.mathieu.2@courrier.uqam.ca>
2019-04-20 15:00:42 -0400

    new: visible radius on hover (only current)

--- Assets/Scripts/Ally/HoverTower.cs
+++ Assets/Scripts/Ally/HoverTower.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class HoverTower : MonoBehaviour
 {
     
-    AttackEnemy ae;
+    private AttackEnemy ae;
+    private static CanvasGroup radiusZone;
     public static bool forUpgrade {get; set;} = false;
 
     void Start(){
         ae = GetComponent<AttackEnemy>();
+        radiusZone = GameObject.Find("RadiusZone").GetComponent<CanvasGroup>();
     }
 
     void OnMouseEnter(){
@@ -21,9 +23,16 @@ public class HoverTower : MonoBehaviour
             AttackEnemy upgrade = factory.Build(ae.towerName,ae.upgradeIndex)?.GetComponent<AttackEnemy>();
             GameObject.FindObjectOfType<StatsPanel>().Display(ae, upgrade);
         }
+        radiusZone.transform.position = transform.position;
+        radiusZone.transform.position += new Vector3(0f,0.6f,0f);
+        float r = 2*ae.radius/15;
+        radiusZone.GetComponentInChildren<UnityEngine.UI.Image>().transform.localScale = new Vector3(r,r,r);
+        radiusZone.alpha = 1f;
     }
     void OnMouseExit(){
         GameObject.FindObjectOfType<StatsPanel>().Hide();
+        radiusZone.GetComponentInChildren<UnityEngine.UI.Image>().transform.localScale = new Vector3(1f,1f,1f);
+        radiusZone.alpha = 0f;
     }
 }
 
